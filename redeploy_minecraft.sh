fa63fba438375e9a76f4d674666d8751c00daf0b
wilgao09 <william.gao09@gmail.com>
2026-01-31 17:16:05 -0600

    Fix ports

--- redeploy_minecraft.sh
+++ redeploy_minecraft.sh
@@ -1,5 +1,8 @@
 ## reset directory to main
 git fetch --all && git reset --hard main && git pull
 
+## make this file execvutable
+chmod +x redeploy_minecraft.sh
+
 ## redeploy
 helm upgrade my-first-mc -f minecraft.yaml minecraft-server-charts/minecraft
